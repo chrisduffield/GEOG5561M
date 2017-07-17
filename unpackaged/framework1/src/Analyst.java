4a9b9da2482ea9edc4b1836130f9e1782633bfeb
chrisduffield <chris@elcatech.co.uk>
2017-07-17 21:12:07 +0100

    End of practical 3

--- unpackaged/framework1/src/Analyst.java
+++ unpackaged/framework1/src/Analyst.java
@@ -2,42 +2,22 @@ import java.util.Arrays;
 
 public class Analyst {
 
-    public static void main(String[] args) {
+    public Analyst() {
 
         Storage store = new Storage();
-    // Loop to initialise array with a random number between 0 and 1000
-        for (int i = 0; i < store.data.length; i++ ){
-            for (int j = 0; j < store.data[i].length; j++) {
-                double var = Math.random() * 1000;
-                store.data[i][j] = var ;
-            }
-        }
-    /*
-    Create a variable to hold the maximum value and then loop checking if the current value is higher
-    and replacing it if it is.
-    */
-        double maximum = -1.0; // this is lower than the potential values in the array.
 
-        for (int i = 0; i < store.data.length; i++ ){
-            for (int j = 0; j < store.data[i].length; j++) {
+        store.setRandomData();
+        store.printArray();
+        System.out.println("Maximum value: " + store.getMaximum());
+        System.out.println("Maximum value: " + store.getMinimum());
 
-                if (store.data[i][j] > maximum) {maximum = store.data[i][j];}
-            }
-        }
-    /*
-    Create a variable to hold the minimum value and then loop checking if the current value is lower
-    and replacing it if it is.
-    */
-        double minimum = 1001.0; // this is lower than the potential values in the array.
-
-        for (int i = 0; i < store.data.length; i++ ){
-            for (int j = 0; j < store.data[i].length; j++) {
-
-                if (store.data[i][j] < minimum) {minimum = store.data[i][j];}
-            }
-        }
-
-        System.out.println("The maximum value is: " + maximum);
-        System.out.println("The minimum value is: " + minimum);
     }
+
+    public static void main(String[] args) {
+        new Analyst();
+
+
+
+    }
+
 }
--- unpackaged/framework1/src/Storage.java
+++ unpackaged/framework1/src/Storage.java
@@ -2,6 +2,60 @@
  * Created by ChrisDuffield on 09/07/2017.
  */
 public class Storage {
-    double[][] data = new double[300][300];
+    double[][] data = new double[10][10];
+
+    void  setRandomData() {
+        // Loop to initialise array with a random number between 0 and 1000
+        for (int i = 0; i < data.length; i++ ){
+            for (int j = 0; j < data[i].length; j++) {
+                double var = Math.random() * 1000;
+                data[i][j] = var ;
+            }
+        }
+     }
+
+    void printArray() {
+        for (int i = 0; i < data.length; i++ ){
+            for (int j = 0; j < data[i].length; j++) {
+
+                System.out.print(data[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    double getMaximum() {
+        /*
+        Create a variable to hold the maximum value and then loop checking if the current value is higher
+        and replacing it if it is.
+        */
+        double maximum = -1.0; // this is lower than the potential values in the array.
+
+        for (int i = 0; i < data.length; i++ ){
+            for (int j = 0; j < data[i].length; j++) {
+
+                if (data[i][j] > maximum) {maximum = data[i][j];}
+            }
+        }
+        return maximum;
+    }
+
+    double getMinimum() {
+        /*
+        Create a variable to hold the minimum value and then loop checking if the current value is lower
+        and replacing it if it is.
+        */
+        double minimum = 1001.0; // this is lower than the potential values in the array.
+
+        for (int i = 0; i < data.length; i++ ){
+            for (int j = 0; j < data[i].length; j++) {
+
+                if (data[i][j] < minimum) {minimum = data[i][j];}
+            }
+        }
+
+        return minimum;
+    }
 
 }
